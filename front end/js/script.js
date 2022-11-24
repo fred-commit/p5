0f9ae37ddcc055a9cf1f8aa763c4ff9863940b0f
Fred.Scoa <85695806+fred-commit@users.noreply.github.com>
2022-11-24 10:32:12 +0100

    Update script.js

--- front end/js/script.js
+++ front end/js/script.js
@@ -1,8 +1,8 @@
-//Récupération du tableau de produits disponibles
+//Récupérer du tableau de produits disponibles
 getProducts();
 
 
-//Création des articles via la liste récupérée précédemment
+//La création des articles via la liste récupérée précédemment
 creationProducts();
 
 async function getProducts() {
@@ -16,28 +16,28 @@ async function creationProducts() {
     .then( (product) => {
         for (let i=0; i < product.length; i++) {		
 
-            // Insertion de l'élément "a"
+            //l'insertion de l'élément "a"
             let productLink = document.createElement("a");
             document.querySelector(".items").appendChild(productLink);
             productLink.href = `product.html?id=${product[i]._id}`;
 
-            // Insertion de l'élément "article"
+            //l'insertion de l'élément "article"
             let productArticle = document.createElement("article");
             productLink.appendChild(productArticle);
 
-            // Insertion de l'image
+            //l'insertion de l'image
             let productImg = document.createElement("img");
             productArticle.appendChild(productImg);
             productImg.src = product[i].imageUrl;
             productImg.alt = product[i].altTxt;
 
-            // Insertion du titre "h3"
+            //l'insertion du titre "h3"
             let productName = document.createElement("h3");
             productArticle.appendChild(productName);
             productName.classList.add("productName");
             productName.innerHTML = product[i].name;
 
-            // Insertion de la description "p"
+            //l'insertion de la description "p"
             let productDescription = document.createElement("p");
             productArticle.appendChild(productDescription);
             productDescription.classList.add("productName");
